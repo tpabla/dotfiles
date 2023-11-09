4413f5d8770b6023515e02621da73a3692991638
Taran Pabla <tpabla@gmail.com>
2023-11-08 23:02:10 -0800

    remove animate due to flickering buffers

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -9,9 +9,7 @@ return {
     require("mini.ai").setup({})
     require("mini.operators").setup({})
     require("mini.surround").setup({})
-    require("mini.animate").setup({})
     require("mini.hipatterns").setup({})
-    -- require("mini.hues").setup({})
     require("mini.move").setup({})
   end,
 }
