1ebaba13f2a0ac041727f8baf929537acf917242
Taran Pabla <tpabla@gmail.com>
2023-11-28 11:55:20 -0800

    Add quick dismiss for noice messages using "\\"

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -22,3 +22,5 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set('n', '\\\\', '<Cmd>Noice dismiss<CR>')
