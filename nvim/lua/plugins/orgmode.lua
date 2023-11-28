a3cfc175e5f73fb0623bff06efda075b904af805
Taran Pabla <tpabla@gmail.com>
2023-11-28 10:35:58 -0800

    modify org-mode toggle settings

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -26,6 +26,11 @@ return {
     require('orgmode').setup({
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
+      mappings = {
+        org = {
+          org_toggle_checkbox = '<leader>t',
+        },
+      },
     })
   end,
 }
