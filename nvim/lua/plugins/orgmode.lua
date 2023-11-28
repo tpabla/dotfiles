52c3f09ae745a821a71ac7e60713ca6a1aec5769
Taran Pabla <tpabla@gmail.com>
2023-11-28 12:38:42 -0800

    Make orgmode more suited to my tastes

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter', lazy = true },
     { 'akinsho/org-bullets.nvim'},
+    { "folke/which-key.nvim" },
   },
   ft = { 'org' },
   event = 'VeryLazy',
@@ -26,11 +27,46 @@ return {
     require('orgmode').setup({
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
+
+      prefix = '<Leader>o',
       mappings = {
+        agenda = {
+          org_agenda_set_tags = '<prefix>T',
+        },
         org = {
-          org_toggle_checkbox = '<leader>t',
+          org_toggle_checkbox = '<leader>x',
+          org_todo = '<prefix>tl',
+          org_todo_prev = '<prefix>th',
+          org_deadline = '<prefix>sd',
+          org_schedule = '<prefix>ss',
+          org_time_stamp = '<prefix>st.',
+          org_clock_in = '<prefix>sc',
+          org_clock_out = '<prefix>sC',
+          org_clock_cancel = '<prefix>sx',
+          org_set_tags_command = '<prefix>T',
         },
       },
+    })
+
+    local wk = require("which-key")
+    wk.register({
+      t = {
+        name = "TODO Methods",
+        h = "Previous TODO State",
+        l = "Next TODO State"
+      },
+      s = {
+        name = "Scheduling TODO's",
+        s = "Schedule",
+        d = "Deadline",
+        t = "Timestamp",
+        c = "Clock in",
+        C = "Clock Out",
+        x = "Clock Cancel",
+      }
+    },
+    {
+      prefix = "<leader>o"
     })
   end,
 }
