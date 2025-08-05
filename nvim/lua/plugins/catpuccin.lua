faaae5bc5db2f1a3d7bc42be74c9c0a197d016db
Taran Pabla <tpabla@gmail.com>
2025-08-04 23:18:51 -0700

    Remove float boarder for better ui

--- nvim/lua/plugins/catpuccin.lua
+++ nvim/lua/plugins/catpuccin.lua
@@ -1,19 +1,18 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
     require("catppuccin").setup({
-        flavour = "macchiato", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-            light = "latte",
-            dark = "macchiato",
-        },
-        transparent_background = true, -- disables setting the background color.
-        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      flavour = "macchiato",   -- latte, frappe, macchiato, mocha
+      background = {           -- :h background
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true,   -- disables setting the background color.
+      term_colors = true,              -- sets terminal colors (e.g. `g:terminal_color_0`)
     })
     vim.cmd.colorscheme "catppuccin"
-    vim.api.nvim_set_hl(0, "FloatBorder", { bg="#3c3836" })
-    end,
+  end,
 }
