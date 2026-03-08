ee5e7eb47b9cf7ff9bb10c2a313467219f81de08
Taran Pabla <tpabla@gmail.com>
2026-03-07 18:04:46 -0800

    updated diffview commands

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,8 +1,11 @@
 return {
     'sindrets/diffview.nvim',
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
     keys = {
         { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
         { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+        { "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Repo History" },
+        { "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
     },
     config = function()
         require("diffview").setup({
